d75be9c237234cc4f91b5072fd74617cd084f3fc
Ivan Gagarinov <dzencot@gmail.com>
2021-05-16 11:32:37 +0500

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe727e9fe45e4d36d6270effad5d005c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ef8c51c-5df5-4bd5-bb2f-1178fc621204'
+};
