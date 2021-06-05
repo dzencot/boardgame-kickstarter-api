5b71a1dc4a0162ff02d3dbb40e15ad97104659df
Ivan Gagarinov <dzencot@gmail.com>
2021-06-05 16:29:29 +0500

    add project-contract

--- api/project-contract/controllers/project-contract.js
+++ api/project-contract/controllers/project-contract.js
@@ -1,8 +1,33 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    const user = await strapi.plugins[
+      'users-permissions'
+    ].services.jwt.getToken(ctx);
+    const projectContract = {
+      ...ctx.request.body,
+      user: user.id,
+      status: 'waiting',
+      contract_date: new Date(),
+    };
+    const entityExist = await strapi.services['project-contract'].findOne({ project: projectContract.project, user: user.id });
+    const entity = entityExist ?
+      await strapi.services['project-contract'].update({ id: entityExist.id }, entityExist, { ...projectContract, updated_by: user.id }) :
+      await strapi.services['project-contract'].create({ ...projectContract, created_by: user.id, updated_by: user.id });
+    return sanitizeEntity(entity, { model: strapi.models['project-contract'] });
+  },
+};
