7068b0090eaead2f41d71250ad45f709c6e8f6bf
Ivan Gagarinov <dzencot@gmail.com>
2021-06-19 13:05:32 +0500

    fix username

--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -5,4 +5,18 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  findOne: async (ctx) => {
+    const projectContract = await strapi.services.project.findOne({ id: ctx.params.id });
+    const contracts = projectContract.project_contracts;
+    const users = await strapi.query('user', 'users-permissions').find({ id: contracts.map(({ user }) => user) }, ['role']);
+    const updatedContracts = contracts.map((contract) => {
+      const currentUser = users.find(({ id }) => id === contract.user);
+      if (!currentUser) {
+        return contract;
+      }
+      return { ...contract, user: { id: currentUser.id, username: currentUser.username } };
+    });
+    return { ...projectContract, project_contracts: updatedContracts };
+  },
+};
